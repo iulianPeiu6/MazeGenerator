d0f39cb032de8f6d2ac5ef84653b56646692613a
Iulian Peiu <iulianpeiu6@gmail.com>
2021-05-03 00:51:52 +0300

    Implement a retained model for maze drawing

--- src/main/java/controllers/GenerateMazeController.java
+++ src/main/java/controllers/GenerateMazeController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
+import models.Cell;
 import models.Coordinate;
 import models.Maze;
 
@@ -29,38 +30,23 @@ public class GenerateMazeController {
     }
 
     private Maze maze;
-    private double lineWidth = 2.5;
     private GraphicsContext graphicsContext;
 
     @FXML
     void generateMaze(ActionEvent event) {
         graphicsContext = mazeCanvas.getGraphicsContext2D();
-        int cellDimension = Integer.parseInt(cellDimensionTextArea.getText());
-        maze = new Maze((int)mazeCanvas.getWidth(), (int)mazeCanvas.getHeight(), cellDimension);
 
         initMaze();
-        iterateMaze(3, 1);
+        drawMaze();
     }
 
     private void initMaze() {
-        graphicsContext.setFill(Color.GRAY);
-        graphicsContext.fillRect(0, 0, mazeCanvas.getWidth(), mazeCanvas.getHeight());
+        int cellDimension = Integer.parseInt(cellDimensionTextArea.getText());
+        maze = new Maze((int)mazeCanvas.getWidth(), (int)mazeCanvas.getHeight(), cellDimension);
+        maze.setStart(new Coordinate((int) (Math.random()* maze.width), (int) (Math.random()* maze.height)));
 
-        graphicsContext.setStroke(Color.WHITE);
-        graphicsContext.setLineWidth(lineWidth);
-
-        for (int i=0; i <= maze.height+1; i++)
-            graphicsContext.strokeLine(0,i* maze.cellDimension , mazeCanvas.getWidth(), i* maze.cellDimension);
-
-        for (int i=0; i <= maze.width+1; i++)
-            graphicsContext.strokeLine(i* maze.cellDimension,0 , i* maze.cellDimension, mazeCanvas.getHeight());
-    }
-
-    private void iterateMaze(int startX, int startY) {
-        graphicsContext.setStroke(Color.GRAY);
-        graphicsContext.setLineWidth(lineWidth+0.5);
         List<Coordinate> cellsStack = new ArrayList<>();
-        Coordinate current = new Coordinate(startX, startY);
+        Coordinate current = maze.getStart();
         cellsStack.add(current);
         maze.setVisitedCell(current);
 
@@ -79,23 +65,50 @@ public class GenerateMazeController {
         }
     }
 
+    private void drawMaze() {
+        graphicsContext.setStroke(Color.valueOf("#0b0054"));
+        graphicsContext.fillRect(0,0,mazeCanvas.getWidth(),mazeCanvas.getHeight());
+        for (int i=0;i<= maze.width;i++)
+            for (int j=0;j<= maze.height;j++)
+                drawCell(maze.cells[i][j], new Coordinate(i,j));
+    }
+
+    private void drawCell(Cell cell, Coordinate coordinate) {
+
+        graphicsContext.setStroke(Color.WHITE);
+        graphicsContext.setLineWidth(2.5);
+
+        if (cell.wallExists("top"))
+            graphicsContext.strokeLine(coordinate.x * maze.cellDimension,
+                    coordinate.y * maze.cellDimension,
+                    (coordinate.x +1) * maze.cellDimension,
+                    coordinate.y * maze.cellDimension);
+        if (cell.wallExists("bottom"))
+            graphicsContext.strokeLine(coordinate.x * maze.cellDimension,
+                    (coordinate.y+1) * maze.cellDimension,
+                    (coordinate.x +1) * maze.cellDimension,
+                    (coordinate.y+1) * maze.cellDimension);
+        if (cell.wallExists("left"))
+            graphicsContext.strokeLine(coordinate.x * maze.cellDimension,
+                    coordinate.y * maze.cellDimension,
+                    coordinate.x * maze.cellDimension,
+                    (coordinate.y+1) * maze.cellDimension);
+        if (cell.wallExists("right"))
+            graphicsContext.strokeLine((coordinate.x+1) * maze.cellDimension,
+                    coordinate.y * maze.cellDimension,
+                    (coordinate.x+1) * maze.cellDimension,
+                    (coordinate.y+1) * maze.cellDimension);
+    }
+
     private void removeWallBetween(int fromX, int fromY, int toX, int toY) {
 
         if (fromX == toX && fromY+1 == toY){
             maze.cells[fromX][fromY].removeWall("bottom");
             maze.cells[toX][toY].removeWall("top");
-            graphicsContext.strokeLine(fromX* maze.cellDimension + lineWidth,
-                    (fromY+1)* maze.cellDimension,
-                    (toX+1)* maze.cellDimension - lineWidth,
-                    toY* maze.cellDimension);
         }
         if (fromY == toY && fromX+1==toX){
             maze.cells[fromX][fromY].removeWall("right");
             maze.cells[toX][toY].removeWall("left");
-            graphicsContext.strokeLine((fromX+1)* maze.cellDimension,
-                    fromY* maze.cellDimension + lineWidth,
-                    toX* maze.cellDimension,
-                    (toY+1)* maze.cellDimension - lineWidth);
         }
         if (fromX == toX && toY+1 == fromY)
             removeWallBetween(toX,toY,fromX,fromY);
